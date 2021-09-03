b49fb2e109b509e134100c6bb10d0d91f83f167a
Duncan Britt <dbru997@gmail.com>
2021-09-02 22:07:09 -0600

    CMS views files from links on home page

--- cms.rb
+++ cms.rb
@@ -9,6 +9,24 @@ configure do
   set :erb, :escape_html => true
 end
 
+before do
+  @files = Dir.glob('data/*.txt').map do |path|
+    name = File.basename(path)
+    { path: path, name: name }
+  end
+end
+
 get '/' do
-  'Getting started.'
+  erb :index, layout: :layout
+end
+
+def file_data_by_name(file_name)
+  @files.find { |info| info[:name] == file_name }
+end
+
+get '/data/:file_name' do
+  file_name = params[:file_name]
+  content_type :text
+  file = file_data_by_name(file_name)
+  File.read(file[:path])
 end
